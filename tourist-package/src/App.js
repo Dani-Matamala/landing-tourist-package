512bcd0dfa48940201773ece4a9d0a1a1164e6df
Matamala Daniel <daniel20mat@gmail.com>
2022-12-14 15:20:10 -0300

    solucionando problemas de estilo con material ui

--- tourist-package/src/App.js
+++ tourist-package/src/App.js
@@ -2,14 +2,15 @@ import { Banner } from "./components/Banner";
 import { Explanation } from "./components/Explanation";
 import { Presentation } from "./components/Presentation";
 import { StickyFooter } from "./components/StickyFooter";
+import { Layout } from "./components/Layout"
 function App() {
   return (
-    <>
+    <Layout>
       <Banner />
       <Explanation />
       <Presentation />
       <StickyFooter />
-    </>
+    </Layout>
   );
 }
 
---
+++ tourist-package/src/components/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { styled } from "@mui/material/styles"
+
+export const Layout = ({children}) => {
+  return (
+    <CustomLayout>
+      {children}
+    </CustomLayout>
+  )
+}
+
+const CustomLayout = styled("div")(({ theme }) => ({
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    minHeight: "100vh",
+    margin: theme.spacing(2)
+}))
+
